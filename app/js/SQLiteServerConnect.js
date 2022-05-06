00cc5a8233e25e7d8be722e6f4bb47057708c7b5
ibrahemesam <iimosa7777@gmail.com>
2022-05-06 08:19:29 +0200

    recovery from failure

--- app/js/SQLiteServerConnect.js
+++ app/js/SQLiteServerConnect.js
@@ -30,28 +30,6 @@ SQLiteServer = {
             return eval('('+data+')');
         }
         return obj.query;
-    },
-    search_local_network: (pong)=>{
-        //pong is the msg that must be contained in the ping response [eg: phamily-SQL]
-        ports = range(1025, 65536).reverse();
-        for (let p = 0; p < ports.length; p++){
-            var ws = await (new Promise((resolve, decline)=>{
-                var ws = new WebSocket(`ws://${ip}:${ports[p]}/`);
-                ws.onopen = e=>{ws.send(secret); resolve(ws)}
-                ws.onerr = e=>{decline(e.target.readyState)}
-
-                ws.onmessage = e=>{
-                    if (e.data.includes(pong)){
-                        resolve(ws);
-                    }else{
-                        resolve(-1);
-                    }
-                }
-            }))
-            if ((typeof ws)==='object'){
-                return [ips[i], ports[p]]
-            }
-        }
     }
 }
 
