358507df8c0a52e490b3e3f7907e24c8148a0cc6
ibrahemesam <iimosa7777@gmail.com>
2022-05-06 09:20:32 +0200

    SQLiteServer: make python client interface

--- SQLiteServer.py
+++ SQLiteServer.py
@@ -38,7 +38,12 @@ class Server:
             # raise Exception("UnAuthorized client")
         while True:
             try:
-                query = await websocket.recv()
+                msg = await websocket.recv()
+                # if query is ping, then pong
+                if msg == 'ping':
+                    await websocket.send(self.pong)
+                    continue
+                else: query = msg
                 # print('query:', query)
                 try:
                     # execute query
@@ -66,7 +71,7 @@ class Server:
 
                     # if first item doesn't exist then data is empty
                     else:
-                        await websocket.send('null')
+                        await websocket.send('None')
 
                     cursor.close() # close the cursor
                 except Exception as e:
@@ -81,17 +86,65 @@ class Server:
                 self.goodbye(websocket, e)
                 return
                 
-    
     def goodbye(self, websocket, exception=None):
         if exception: print(exception)
         print('goodbye <3')
         return
+
+    class connect:
+        ###
+        # client reqires: pip install websocket-client
+        ###
+        def __init__(self, ip, port, secret):
+            from websocket import create_connection
+            self.ws = create_connection(f"ws://{ip}:{port}")
+            # authentication
+            self.ws.send(secret)
+            # keep alive
+            self.keep_alive()
+
+        def query(self, query):
+            self.ws.send(query)
+            data = self.ws.recv()
+            if data.startswith('[Error]:'):
+                raise Exception(data)
+            return eval(data)
+
+        def close(self): self.ws.close()
+
+        def keep_alive(self):
+            # workaround for this error:
+            #     sent 1011 (unexpected error) keepalive ping timeout; no close frame received
+            import time
+            async def _keepalive():
+                while True:
+                    time.sleep(15)
+                    self.ws.send('ping')
+                    self.ws.recv() # recv pong
+            threading.Thread(
+                target=(lambda: asyncio.new_event_loop()\
+                    .run_until_complete(_keepalive()))
+                    , args=()).start()
+            
+
+
     
 
 
 
 if __name__ == '__main__':
     Server('tst.db', 3425)
+    import time
+    time.sleep(1)
+    sql = Server.connect('127.0.0.1', 3425, 'null')
+    print(sql.query('select * from users'))
+    while 1:
+        i = input('>>> ')
+        if i == 'q': break
+        try:
+            print(sql.query(i))
+        except Exception as e:
+            print(e)
 
 #TODO: make JS interface
 #TODO: make PY interface
